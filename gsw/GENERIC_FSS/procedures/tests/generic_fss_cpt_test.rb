67ca3e66c215ba428cf582d79c3851d35d284d4e
Kevin Bruce <kevin.c.bruce@nasa.gov>
2025-03-20 16:57:06 -0400

    [nasa/nos3#228] Some updates to FSS tests. Seems the way of calculating Alpha and Beta from SVB is off.

--- gsw/GENERIC_FSS/procedures/tests/generic_fss_cpt_test.rb
+++ gsw/GENERIC_FSS/procedures/tests/generic_fss_cpt_test.rb
@@ -1,6 +1,7 @@
 require 'cosmos'
 require 'cosmos/script'
 require "cfs_lib.rb"
+#require 'math'
 
 ##
 ## NOOP
@@ -10,58 +11,6 @@ initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
 initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
 cmd("GENERIC_FSS GENERIC_FSS_NOOP_CC")
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-
-sleep(5)
-
-##
-## Successful Enable
-##
-initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
-initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_FSS GENERIC_FSS_ENABLE_CC")
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
-
-sleep(5)
-
-##
-## Failed Enable (doubled)
-##
-initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
-initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_FSS GENERIC_FSS_ENABLE_CC")
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
-
-sleep(5)
-
-##
-## Housekeeping w/ Device
-##
-initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_FSS GENERIC_FSS_REQ_HK")
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-
-sleep(5)
-
-##
-## Data w/ Device
-##
-initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_FSS GENERIC_FSS_REQ_DATA")
-truth_42_alpha = tlm("")
-truth_42_beta = tlm("")
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
 
@@ -114,6 +63,68 @@ initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUN
 cmd("GENERIC_FSS GENERIC_FSS_REQ_DATA")
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Successful Enable
+##
+initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_ENABLE_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Failed Enable (doubled)
+##
+initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_ENABLE_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Housekeeping w/ Device
+##
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_REQ_HK")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Data w/ Device
+##
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_REQ_DATA")
+fss_alpha = tlm("GENERIC_FSS GENERIC_FSS_DATA_TLM GENERIC_FSS_ALPHA")
+fss_beta = tlm("GENERIC_FSS GENERIC_FSS_DATA_TLM GENERIC_FSS_BETA")
+
+svb0 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_0")
+svb1 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_1")
+svb2 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_2")
+
+truth_42_alpha = Math.atan2(svb2, svb0)
+truth_42_beta = Math.atan2(svb2, svb1)
+
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check_expression("fss_alpha == truth_42_alpha # #{fss_alpha} == #{truth_42_alpha}", 15)
+wait_check_expression("fss_beta == truth_42_beta # #{fss_beta} == #{truth_42_beta}", 15)
 
 sleep(5)
 
