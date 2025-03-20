a7abf2d93d27f6c258d86ba1b54608215254ee31
Kevin Bruce <kevin.c.bruce@nasa.gov>
2025-03-20 11:10:52 -0400

    [nasa/nos3#228] updates to FSS system test prototype

--- gsw/GENERIC_FSS/procedures/tests/generic_fss_lpt_test.rb
+++ gsw/GENERIC_FSS/procedures/tests/generic_fss_lpt_test.rb
@@ -2,25 +2,124 @@ require 'cosmos'
 require 'cosmos/script'
 require "cfs_lib.rb"
 
+##
+## NOOP
+##
+initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_NOOP_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
 
-# initial_command_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_COUNT")
-# initial_error_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT")
-# initial_device_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT")
-# initial_device_error_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT")
-# cmd("SAMPLE SAMPLE_NOOP_CC")
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT == #{initial_device_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+sleep(5)
 
-# sleep(5)
+##
+## Successful Enable
+##
+initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_ENABLE_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
 
-# initial_command_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_COUNT")
-# initial_error_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT")
-# initial_device_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT")
-# initial_device_error_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT")
-# cmd("SAMPLE SAMPLE_RST_COUNTERS_CC")
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT == 0", 30)
+sleep(5)
+
+##
+## Failed Enable (doubled)
+##
+initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_ENABLE_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Housekeeping w/ Device
+##
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_REQ_HK")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Data w/ Device
+##
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_REQ_DATA")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Successful Disable
+##
+initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_DISABLE_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'DISABLED'", 30)
+
+sleep(5)
+
+##
+## Failed Disable (doubled)
+##
+initial_command_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_DISABLE_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ENABLED == 'DISABLED'", 30)
+
+sleep(5)
+
+##
+## HK without Device
+##
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_REQ_HK")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Data without Device
+##
+initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_FSS GENERIC_FSS_REQ_DATA")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Reset Counters
+##
+cmd("GENERIC_FSS GENERIC_FSS_RST_COUNTERS_CC")
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT == 0", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == 0", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_COUNT == 0", 30)
+wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == 0", 30)
