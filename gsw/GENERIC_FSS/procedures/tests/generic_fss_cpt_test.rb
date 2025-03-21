c3214ccd4cdbc4184da1d1207b08c1db60eeb958
Kevin Bruce <kevin.c.bruce@nasa.gov>
2025-03-21 14:39:56 -0400

    [nasa/nos3#228] Attempts to fix FSS Data verification (working better, but still not consistently. Not sure what is off, or if I just need bigger margins on Alpha. Beta seems quite off (though better than before))

--- gsw/GENERIC_FSS/procedures/tests/generic_fss_cpt_test.rb
+++ gsw/GENERIC_FSS/procedures/tests/generic_fss_cpt_test.rb
@@ -110,29 +110,48 @@ sleep(5)
 ##
 initial_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT")
 initial_device_error_count = tlm("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT")
+
 cmd("GENERIC_FSS GENERIC_FSS_REQ_DATA")
-fss_alpha = tlm("GENERIC_FSS GENERIC_FSS_DATA_TLM GENERIC_FSS_ALPHA")
-fss_beta = tlm("GENERIC_FSS GENERIC_FSS_DATA_TLM GENERIC_FSS_BETA")
 
 svb0 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_0")
 svb1 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_1")
 svb2 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_2")
 
-truth_42_alpha = Math.atan2(svb2, svb0)
-truth_42_beta = Math.atan2(svb2, svb1)
+fss_alpha = tlm("GENERIC_FSS GENERIC_FSS_DATA_TLM GENERIC_FSS_ALPHA")
+fss_beta = tlm("GENERIC_FSS GENERIC_FSS_DATA_TLM GENERIC_FSS_BETA")
+fss_error = tlm("GENERIC_FSS GENERIC_FSS_DATA_TLM GENERIC_FSS_ERROR_CODE")
+
+truth_42_alpha = -Math.atan2(svb2, svb0)
+truth_42_beta = Math.atan2(svb1, svb2)
+
+truth_42_alpha_upper = truth_42_alpha + 0.01
+truth_42_alpha_lower = truth_42_alpha - 0.01
+truth_42_beta_upper = truth_42_beta + 0.01
+truth_42_beta_lower = truth_42_beta - 0.01
 
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-wait_check_expression("fss_alpha == truth_42_alpha # #{fss_alpha} == #{truth_42_alpha}", 15)
-wait_check_expression("fss_beta == truth_42_beta # #{fss_beta} == #{truth_42_beta}", 15)
+if fss_error == 0
+  wait_check_expression("fss_alpha <= truth_42_alpha_upper # #{fss_alpha} <= #{truth_42_alpha_upper}", 15)
+  wait_check_expression("fss_alpha >= truth_42_alpha_lower # #{fss_alpha} >= #{truth_42_alpha_lower}", 15)
+
+  wait_check_expression("fss_beta <= truth_42_beta_upper # #{fss_beta} <= #{truth_42_beta_upper}", 15)
+  wait_check_expression("fss_beta >= truth_42_beta_lower # #{fss_beta} >= #{truth_42_beta_lower}", 15)
+end
 
 sleep(5)
 
 ##
 ## Reset Counters
 ##
+cmd("GENERIC_FSS GENERIC_FSS_DISABLE_CC") # disable to be able to properly check that device count gets reset
 cmd("GENERIC_FSS GENERIC_FSS_RST_COUNTERS_CC")
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_COUNT == 0", 30)
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM CMD_ERR_COUNT == 0", 30)
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_COUNT == 0", 30)
 wait_check("GENERIC_FSS GENERIC_FSS_HK_TLM DEVICE_ERR_COUNT == 0", 30)
+
+##
+## Reenable so test can be run multiple times
+##
+cmd("GENERIC_FSS GENERIC_FSS_ENABLE_CC")
