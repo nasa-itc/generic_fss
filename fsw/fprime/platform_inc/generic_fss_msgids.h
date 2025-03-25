678004a421fe5d48894cb91adb2c17b389c86352
rjbrown6 <Robert.J.Brown@nasa.gov>
2025-03-25 12:08:15 -0400

    Revert "style: auto-format via clang-format"
    This reverts commit 3682a8931bf09a5c03fcf032ba68e34b9d03048f.

--- fsw/fprime/platform_inc/generic_fss_msgids.h
+++ fsw/fprime/platform_inc/generic_fss_msgids.h
@@ -9,20 +9,20 @@
 #ifndef _GENERIC_FSS_MSGIDS_H_
 #define _GENERIC_FSS_MSGIDS_H_
 
-/*
+/* 
 ** CCSDS V1 Command Message IDs (MID) must be 0x18xx
 */
-#define GENERIC_FSS_CMD_MID 0x1920
+#define GENERIC_FSS_CMD_MID              0x1920
 
-/*
+/* 
 ** This MID is for commands telling the app to publish its telemetry message
 */
-#define GENERIC_FSS_REQ_HK_MID 0x1921
+#define GENERIC_FSS_REQ_HK_MID           0x1921
 
-/*
+/* 
 ** CCSDS V1 Telemetry Message IDs must be 0x08xx
 */
-#define GENERIC_FSS_HK_TLM_MID 0x0920
-#define GENERIC_FSS_DEVICE_TLM_MID 0x0921
+#define GENERIC_FSS_HK_TLM_MID           0x0920
+#define GENERIC_FSS_DEVICE_TLM_MID       0x0921
 
 #endif /* _GENERIC_FSS_MSGIDS_H_ */
