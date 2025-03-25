4df108c5e96f9229a218216b26dabfcd6b59bc02
rjbrown2 <91291114+rjbrown2@users.noreply.github.com>
2025-03-25 16:09:41 +0000

    style: auto-format via clang-format

--- fsw/fprime/src/generic_fss_msg.h
+++ fsw/fprime/src/generic_fss_msg.h
@@ -15,16 +15,16 @@
 /*
 ** Ground Command Codes
 */
-#define GENERIC_FSS_NOOP_CC                 0
-#define GENERIC_FSS_RESET_COUNTERS_CC       1
-#define GENERIC_FSS_ENABLE_CC               2
-#define GENERIC_FSS_DISABLE_CC              3
+#define GENERIC_FSS_NOOP_CC           0
+#define GENERIC_FSS_RESET_COUNTERS_CC 1
+#define GENERIC_FSS_ENABLE_CC         2
+#define GENERIC_FSS_DISABLE_CC        3
 
-/* 
+/*
 ** Telemetry Request Command Codes
 */
-#define GENERIC_FSS_REQ_HK_TLM              0
-#define GENERIC_FSS_REQ_DATA_TLM            1
+#define GENERIC_FSS_REQ_HK_TLM   0
+#define GENERIC_FSS_REQ_DATA_TLM 1
 
 /*
 ** Generic "no arguments" command type definition
@@ -38,25 +38,25 @@ typedef struct
 /*
 ** GENERIC_FSS device telemetry definition
 */
-typedef struct 
+typedef struct
 {
-    CFE_MSG_TelemetryHeader_t TlmHeader;
+    CFE_MSG_TelemetryHeader_t     TlmHeader;
     GENERIC_FSS_Device_Data_tlm_t Generic_fss;
 } __attribute__((packed)) GENERIC_FSS_Device_tlm_t;
-#define GENERIC_FSS_DEVICE_TLM_LNGTH sizeof ( GENERIC_FSS_Device_tlm_t )
+#define GENERIC_FSS_DEVICE_TLM_LNGTH sizeof(GENERIC_FSS_Device_tlm_t)
 
 /*
 ** GENERIC_FSS housekeeping type definition
 */
-typedef struct 
+typedef struct
 {
     CFE_MSG_TelemetryHeader_t TlmHeader;
-    uint8   CommandErrorCount;
-    uint8   CommandCount;
-    uint8   DeviceErrorCount;
-    uint8   DeviceCount;
-    uint8   DeviceEnabled;
+    uint8                     CommandErrorCount;
+    uint8                     CommandCount;
+    uint8                     DeviceErrorCount;
+    uint8                     DeviceCount;
+    uint8                     DeviceEnabled;
 } __attribute__((packed)) GENERIC_FSS_Hk_tlm_t;
-#define GENERIC_FSS_HK_TLM_LNGTH sizeof ( GENERIC_FSS_Hk_tlm_t )
+#define GENERIC_FSS_HK_TLM_LNGTH sizeof(GENERIC_FSS_Hk_tlm_t)
 
 #endif /* _GENERIC_FSS_MSG_H_ */
